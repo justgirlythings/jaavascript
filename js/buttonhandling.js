8f1e4691a712ed295fec91978219790db6e419dc
Virta <frans.ojala@helsinki.fi>
2013-11-06 21:23:54 +0200

    Rakenteen hahmottelua

---
+++ js/buttonhandling.js
@@ -0,0 +1,15 @@
+ $( "#types" ).click(function() {
+	 $( "#typesdesc" ).toggle("show");
+ });
+
+ $( "button" ).click(function() {
+	 $( "#typesdesc" ).toggle("show");
+ });
+
+ $( "#other" ).click(function() {
+	 $( "#otherstuff" ).toggle("show");
+ });
+
+ $( "button" ).click(function() {
+	 $( "#otherstuff" ).toggle("show");
+ });
